920be44ebb2178f92d4e9233f99a27bbfdfae1bc
chenbowen <13775039095@163.com>
2021-08-08 21:35:28 +0800

    master new project create

---
+++ src/main/java/com/cbw/week1/jvm/jvmDemo.java
@@ -0,0 +1,11 @@
+package com.cbw.week1.jvm;
+
+/**
+ * @author chenbowen
+ * @date 2021/8/8 21:34
+ */
+public class jvmDemo {
+    public static void main(String[] args) {
+        System.out.printf("test");
+    }
+}
