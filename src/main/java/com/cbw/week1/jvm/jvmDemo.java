00235b1f1442a270c6be6f0c37beee7e2b847fe2
chenbowen <13775039095@163.com>
2021-08-08 23:05:00 +0800

    master 自定义类加载器

--- src/main/java/com/cbw/week1/jvm/jvmDemo.java
+++ src/main/java/com/cbw/week1/jvm/jvmDemo.java
@@ -1,11 +1,18 @@
 package com.cbw.week1.jvm;
 
+import sun.misc.Launcher;
+
+import java.net.URL;
+import java.util.Arrays;
+
 /**
  * @author chenbowen
  * @date 2021/8/8 21:34
  */
 public class jvmDemo {
     public static void main(String[] args) {
-        System.out.printf("test");
+        URL[] urLs = Launcher.getBootstrapClassPath().getURLs();
+        System.out.printf("启动类加载器");
+        Arrays.stream(urLs).forEach(System.out::println);
     }
 }
